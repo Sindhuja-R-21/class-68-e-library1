bc2e6ac836d1e4e7ff80fe9209d599c410b93615
Sindhuja R <54587799+Sindhuja-R-21@users.noreply.github.com>
2022-04-04 20:44:55 +0530

    Add files via upload

---
+++ components/BottomTabNavigator.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import TransactionScreen from "../screens/Transaction";
+import SearchScreen from "../screens/Search";
+
+
